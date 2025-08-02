d05bd34290a8c66c83831bdcf3a8cab98a3e998c
Prem <premkumarsingh355@gmail.com>
2025-08-02 19:40:23 +0530

    commit "Recovered"

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
 export const VITE_API_URL =
     import.meta.env.MODE === "production"
-        ? "https://cgpa-fetcher.onrender.com"
+        ? "https://cgpa-fetcher-production.up.railway.com"
         : "http://localhost:5000";
